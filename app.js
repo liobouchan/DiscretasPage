637eca1749d2fbefa1fe2a3d593e961a3d48e482
Liobouchan <leo.bouchan@gmail.com>
2013-12-12 01:27:38 -0600

    1

---
+++ app.js
@@ -0,0 +1,40 @@
+/**
+ * Module dependencies.
+ */
+
+var express = require('express')
+  , routes = require('./routes')
+  , user = require('./routes/user')
+  , http = require('http')
+  , path = require('path');
+
+var app = express();
+
+// all environments
+app.set('port', process.env.PORT || 3000);
+app.set('views', __dirname + '/views');
+app.set('view engine', 'jade');
+app.locals.pretty = true;
+app.use(express.favicon());
+app.use(express.logger('dev'));
+app.use(express.bodyParser());
+app.use(express.methodOverride());
+app.use(app.router);
+  app.use(require('less-middleware')({ src: __dirname + '/views' }));
+app.use(express.static(path.join(__dirname, 'views')));
+
+// development only
+if ('development' == app.get('env')) {
+  app.use(express.errorHandler());
+}
+
+app.get('/', routes.index);
+//app.get('/matematicas_discretas/teoria_de_conjuntos', routes.teoria_de_conjuntos);
+//app.get('/matematicas_discretas/maximo_comun_divisor', routes.maximo_comun_divisor);
+//app.get('/matematicas_discretas/orden', routes.orden);
+//app.get('/matematicas_discretas/dijkstra', routes.dijkstra);
+app.get('/users', user.list);
+
+http.createServer(app).listen(app.get('port'), function(){
+  console.log('Express server listening on port ' + app.get('port'));
+});
